b87eedcf657ec9f538d925661aee3f037001025c
Ving Trung <vingtrung@gmail.com>
2019-05-03 20:36:13 -0700

    reorganize

--- config-sample.js
+++
@@ -1,9 +0,0 @@
-// Create a config.js file
-// Copy content of this file into new config.js
-// Set Values to your environment
-
-var config = {};
-
-config.mongoConnectionString = "mongodb://localhost:27017/test";
-config.port = 3000;
-module.exports = config;
